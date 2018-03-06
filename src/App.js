92d4f253fe872370f008ea162c42c115790372a2
Gyokan <gyokan.nezhdetov@gmail.com>
2018-03-06 18:46:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <p className='App-intro'>
+          Hello in Quiz Learner!
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
